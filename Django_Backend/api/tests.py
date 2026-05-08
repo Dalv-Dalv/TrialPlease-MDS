0b6088abc093c960020583c25d041078cdf523f2
stefanbusoi <92371049+stefanbusoi@users.noreply.github.com>
2026-05-08 21:41:52 +0300

    GIthub Action Integration

--- Django_Backend/api/tests.py
+++ Django_Backend/api/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
 
-# Create your tests here.
+class SimpleTest(TestCase):
+    def test_basic_addition(self):
+        """A simple test to ensure the testing framework is working."""
+        self.assertEqual(1 + 1, 2)
